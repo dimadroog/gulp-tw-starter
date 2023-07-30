f4187b6207d1d151d5bac7467bb373bd646925ad
DAGorbachev <DAGorbachev@1cbit.ru>
2023-07-30 14:05:11 +0300

    tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: 'jit',
-    content: ['app/index.html', 'app/scss/*.scss', 'app/js/*.js'],
+    content: [
+        'app/**/*.html', 
+        'app/scss/*.scss', 
+        'app/js/*.js',
+      ],
     theme: {
         extend: {},
     },
-//     purge: [
-//         'app/**/*.html', 
-//         'app/scss/*.scss', 
-//         'app/js/*.js',
-//         'dist/**/*.html', 
-//         'dist/css/*.css', 
-//         'dist/js/*.js'
-// ],
-
     plugins: [],
 };
